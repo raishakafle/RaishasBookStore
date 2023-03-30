222bba3c1c100ff8de34169a46f898c612b511f6
raisakafle <113300872+raisakafle@users.noreply.github.com>
2023-03-30 18:15:42 -0400

    Added IRepository

--- RaishasBooks.DataAccess/Data/ApplicationDbContext.cs
+++ RaishasBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RaishasBooks.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,6 +12,9 @@ namespace RaishasBookStore.DataAccess.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
